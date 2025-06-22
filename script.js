4634e789007ef92b8b52ed82e86f7471d52ebf3b
benxta4424 <benxtaben@gmail.com>
2025-06-23 02:00:22 +0300

    creating the book box dynamically and using the same technique we add the title and author inside the book

--- script.js
+++ script.js
@@ -2,26 +2,19 @@ const myLibrary = []
 
 function Book(author, title) {
     this.id = crypto.randomUUID()
-    this.author = author;
-    this.title = title;
+    this.author = author
+    this.title = title
 } 
 
 function addBookToLibrary(author, title) {
-    new_book = new Book(author, title);
+    new_book = new Book(author, title)
     myLibrary.push(new_book)
     return new_book;
 }
 
-
-const book1 = addBookToLibrary("ben","tun")
-const book2 = addBookToLibrary("dan","sah")
-const book3 = addBookToLibrary("darius","hur")
-const book4 = addBookToLibrary("daniel","grr")
-
-
 function getBook() {
     for(let i = 0; i < myLibrary.length; i++) {
-        document.getElementById("bookTitle").innerHTML = myLibrary[i].name
+        console.log(myLibrary[i].name)
     }
 }
 
@@ -32,5 +25,38 @@ const addBookButton = document.getElementById("add_books");
 const bookForm = document.getElementById("bookForm")
 
 addBookButton.addEventListener("click", ()=> {
-    bookForm.style.display = "block";
+    bookForm.style.display = "block"
 })
+
+
+function appendBookToDom(dynamic_book) {
+    // create the box for the title and author and name it box
+    const createBox = document.createElement("div")
+    createBox.className = "box"
+
+    const title = document.createElement('span')
+    title.className = "title_box"
+    title.textContent = dynamic_book.title
+
+    const author = document.createElement("span")
+    author.className = "author_box"
+    author.textContent = dynamic_book.author
+
+    createBox.appendChild(author)
+    createBox.appendChild(title)
+
+    // append the books to the container
+    const bookContainer = document.getElementById("all_books")    
+    bookContainer.appendChild(createBox)    
+}
+
+bookForm.addEventListener("submit", (e) => {
+    e.preventDefault() 
+
+    const author = document.getElementById("author").value
+    const title = document.getElementById("title").value
+
+    const dynamically_added_book = addBookToLibrary(author, title)
+    appendBookToDom(dynamically_added_book)
+})
+
